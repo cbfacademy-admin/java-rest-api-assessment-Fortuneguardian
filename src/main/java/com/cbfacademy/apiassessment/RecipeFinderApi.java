bdc2ede860f1373a1515bf99db91622f354b4bb3
Binta Hanakuka <fortuneguardian@gmail.com>
2023-10-29 00:23:06 +0100

    Recipe Finder API and Tests

---
+++ src/main/java/com/cbfacademy/apiassessment/RecipeFinderApi.java
@@ -0,0 +1,25 @@
+package com.cbfacademy.apiassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RecipeFinderApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeFinderApi.class, args);
+	}
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s", name);
+	}
+
+}
+
+
+
