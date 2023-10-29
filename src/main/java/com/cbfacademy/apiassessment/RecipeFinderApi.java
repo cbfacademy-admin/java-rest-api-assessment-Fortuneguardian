b17b25344ea3d16972b66ce43ba38f6f0c46ca4b
Binta Hanakuka <fortuneguardian@gmail.com>
2023-10-29 23:49:29 +0000

    added gson.jar to lib, added gson dependency to pom.xml, created Recipes.java class

--- src/main/java/com/cbfacademy/apiassessment/RecipeFinderApi.java
+++ src/main/java/com/cbfacademy/apiassessment/RecipeFinderApi.java
@@ -1,9 +1,14 @@
 package com.cbfacademy.apiassessment;
+import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.io.FileReader;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,12 +19,22 @@ public class RecipeFinderApi {
 		SpringApplication.run(RecipeFinderApi.class, args);
 	}
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
-	}
+	@GetMapping("/recipes")
+    public String getRecipes() {
+        // Initialize a GSON instance
+        Gson gson = new Gson();
 
+        try (FileReader reader = new FileReader("recipes.json")) {
+            // Use GSON to deserialize the JSON file into your Java objects
+            Recipes[] recipes = gson.fromJson(reader, Recipes[].class);
+
+            // Process the recipes as needed
+            // You can return them as JSON or perform other operations here
+
+            return gson.toJson(recipes); // Return the recipes as JSON
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error: Unable to read recipes.";
+        }
+    }
 }
-
-
-
